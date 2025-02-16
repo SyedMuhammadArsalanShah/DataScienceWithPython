3fc0762772d44f4a7babd38477808de78d690060
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2025-02-16 14:42:13 +0500

    code

---
+++ 05loops.py
@@ -0,0 +1,102 @@
+a=1
+
+while a<=10:
+    print("arsalan",a)
+    a=a+1
+
+
+b=1
+while b<=100:
+    if b%2==0:
+        print("even",b)
+    else:
+        print("odd",b)
+    b=b+1
+
+byear=1996
+counter=0
+while byear<=2025:
+    if byear %4==0:
+        print("is leap year", byear)
+        counter=counter+1
+    else:
+        print("is not leap year", byear)
+
+    byear=byear+1
+
+print("leap year counts", counter)
+
+
+i=2
+fact=1
+nnum=int(input("Enter your number "))
+while i<=nnum:
+
+    fact=fact*i
+    i=i+1
+
+
+
+print("factorial is = ",fact)
+
+
+i=1
+sum=0
+nnum=int(input("Enter your number "))
+while i<=nnum:
+
+    sum=sum+i
+    i=i+1
+
+
+
+print("sum  is = ",sum)
+
+
+
+# Table
+
+i = 1
+nnum=int(input("Enter your number for table "))
+
+while i<=10:
+    print(nnum, " x ", i, " = ", nnum*i)
+    i=i+1
+
+students= ["Baneen","Urooj", "Esha", "hafsa","Junaid", "Arshad", "Hamza", "Talha", "Saaria"]
+
+
+for sa in students:
+
+    print(sa)
+
+
+
+for a in range(10):
+
+    print(a)
+
+
+# range (start , stop , step)
+for a in range(10,20,4):
+
+    print(a)
+
+
+
+
+for a in range(11):
+
+    if a==8 or a==9:
+        continue
+    print(a)
+
+
+while True:
+     name= input("Enter Your Name ")
+     if name== "exit":
+       break
+
+
+
+
